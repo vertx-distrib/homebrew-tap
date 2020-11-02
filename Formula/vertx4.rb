7be702e41e6afaa3d381af293bc1ad4be79c1119
Julien Viet <julien@julienviet.com>
2020-11-02 15:18:45 +0100

    Test Vert.x 4 formula for 4.0.0.CR2

---
+++ Formula/vertx4.rb
@@ -0,0 +1,37 @@
+class Vertx4 < Formula
+  desc "Toolkit for building reactive applications on the JVM"
+  homepage "https://vertx.io/"
+  url "https://repo1.maven.org/maven2/io/vertx/vertx-stack-manager/4.0.0.CR2/vertx-stack-manager-4.0.0.CR2-full.zip"
+  sha256 "7883c481badb1725a91e6c411a646f557b369b9593e6e4e4bfeb7ca875ace716"
+  license any_of: ["EPL-2.0", "Apache-2.0"]
+
+  livecheck do
+    url "https://vertx.io/download/"
+    regex(/href=.*?vert\.x[._-]v?(\d+(?:\.\d+)+)-full\.t/i)
+  end
+
+  bottle :unneeded
+
+  depends_on "openjdk"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin conf lib]
+    (bin/"vertx").write_env_script "#{libexec}/bin/vertx", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    (testpath/"HelloWorld.java").write <<~EOS
+      import io.vertx.core.AbstractVerticle;
+      public class HelloWorld extends AbstractVerticle {
+        public void start() {
+          System.out.println("Hello World!");
+          vertx.close();
+          System.exit(0);
+        }
+      }
+    EOS
+    output = shell_output("#{bin}/vertx run HelloWorld.java")
+    assert_equal "Hello World!\n", output
+  end
+end
