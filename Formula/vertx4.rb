6c4ddb2503b3e8a3060b6727a7ed9452b4a0a357
Julien Viet <julien@julienviet.com>
2022-03-16 17:58:00 +0100

    4.2.6 release

--- Formula/vertx4.rb
+++ Formula/vertx4.rb
@@ -1,8 +1,8 @@
 class Vertx4 < Formula
   desc "Toolkit for building reactive applications on the JVM"
   homepage "https://vertx.io/"
-  url "https://repo1.maven.org/maven2/io/vertx/vertx-stack-manager/4.2.5/vertx-stack-manager-4.2.5-full.zip"
-  sha256 "763e5a2d41ebd5a787d393632451cd6b773dcd164810c3cca25b250613e9381e"
+  url "https://repo1.maven.org/maven2/io/vertx/vertx-stack-manager/4.2.6/vertx-stack-manager-4.2.6-full.zip"
+  sha256 "a1264068940cccf0b2d95aedac1d8a3cd082e8d52ef314ec557ce68ae1a40db4"
   license any_of: ["EPL-2.0", "Apache-2.0"]
 
   livecheck do
